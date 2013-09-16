3bcb82b8dfb341465ee5645601b1bea89cb5f292
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-09-16 20:58:15 -0300

    Cambios para soportar la API en la controladora de ususarios

--- PISServer/Controllers/UsersController.cs
+++ PISServer/Controllers/UsersController.cs
@@ -5,48 +5,75 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+// Import models
+using PISServer.Models;
+
 namespace PISServer.Controllers
 {
     public class UsersController : ApiController
     {
         // This holds an IProductRepository instance.
         // This repository holds a collection of Users to handle operations.
-        static readonly PISServer.Models.IUserRepository repository = new PISServer.Models.UserRepository();
+        // TODO: Check this http://www.asp.net/web-api/overview/extensibility/using-the-web-api-dependency-resolver
+        static readonly IUserRepository repository = new UserRepository();
 
         // 
         // GET api/users 
         //
-        public IEnumerable<string> Get()
+        // The method name starts with "Get", so by convention it maps to GET requests. 
+        // Also, because the method has no parameters, it maps to a URI that does 
+        // not contain an "id" segment in the path.
+        public IEnumerable<User> GetAllUsers()
         {
-            return new string[] { "value1", "value2" };
+            return repository.GetAll();
         }
 
         //
-        // GET api/users/5
+        // GET api/users/:id
         //
-        public string Get(int id)
+        // This method name also starts with "Get", but the method has a parameter named id. 
+        // This parameter is mapped to the "id" segment of the URI path. 
+        // The ASP.NET Web API framework automatically converts the ID to the 
+        // correct data type (int) for the parameter.
+        public User GetUser(int id)
         {
-            return "value";
+            User item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
         }
 
         //
         // POST api/users
         //
-        public void Post([FromBody]string value)
+        public HttpResponseMessage PostUser(User item)
         {
+            item = repository.Add(item);
+            var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+            string uri = Url.Link("DefaultApi", new { id = item.Id });
+            response.Headers.Location = new Uri(uri);
+            return response;
         }
 
         //
-        // PUT api/users/5
+        // PUT api/users/:id
         //
-        public void Put(int id, [FromBody]string value)
+        public void PutUser(int id, User user)
         {
+            user.Id = id;
+            if (!repository.Update(user))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
         }
 
         //
         // DELETE api/users/5
         //
-        public void Delete(int id)
+        public void DeleteUser(int id)
         {
         }
     }
