674d926fad9dc31552f92ea18a459a263f7f58d2
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-09-16 19:59:37 -0300

    Comentarios en user controller agregado

--- PISServer/Controllers/UsersController.cs
+++ PISServer/Controllers/UsersController.cs
@@ -9,29 +9,44 @@ namespace PISServer.Controllers
 {
     public class UsersController : ApiController
     {
-        // GET api/users
+        // 
+        // GET api/users 
+        // Returns a list of users from the database
+        //
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
+        //
         // GET api/users/5
+        // Returns a json of a user from a given id
+        //
         public string Get(int id)
         {
             return "value";
         }
 
+        //
         // POST api/users
+        // ?
+        //
         public void Post([FromBody]string value)
         {
         }
 
+        //
         // PUT api/users/5
+        // ?
+        //
         public void Put(int id, [FromBody]string value)
         {
         }
 
+        //
         // DELETE api/users/5
+        // Delete a user from a given id
+        //
         public void Delete(int id)
         {
         }
