dbc561b1a222ed2911e7bdea938ed194c96cf1e5
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-09-21 15:40:31 -0300

    LoginController arreglada para aceptar la ruta de login

--- PISServer/Controllers/LoginController.cs
+++ PISServer/Controllers/LoginController.cs
@@ -12,6 +12,21 @@ namespace PISServer.Controllers
 {
     public class LoginController : ApiController
     {
+        // This holds an IUsertRepository instance.
+        // This repository holds a collection of Users to handle operations.
+        // TODO: Check this http://www.asp.net/web-api/overview/extensibility/using-the-web-api-dependency-resolver
+        static readonly IUserRepository repository = new UserRepository();
 
+        //
+        // GET api/login
+        //
+        // This method name also starts with "Get", but the method has a parameter named id. 
+        // This parameter is mapped to the "id" segment of the URI path. 
+        // The ASP.NET Web API framework automatically converts the ID to the 
+        // correct data type (int) for the parameter.
+        public string GetLogin()
+        {
+            return "item";
+        }
     }
 }
