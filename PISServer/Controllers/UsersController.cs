5e0c071be235781235ce02ef9e9817a38642e4ae
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-09-22 12:44:24 -0300

    Get user modificado y funciones minimas actualizadas

--- PISServer/Controllers/UsersController.cs
+++ PISServer/Controllers/UsersController.cs
@@ -17,17 +17,6 @@ namespace PISServer.Controllers
         // TODO: Check this http://www.asp.net/web-api/overview/extensibility/using-the-web-api-dependency-resolver
         static readonly IUserRepository repository = new UserRepository();
 
-        // 
-        // GET api/users 
-        //
-        // The method name starts with "Get", so by convention it maps to GET requests. 
-        // Also, because the method has no parameters, it maps to a URI that does 
-        // not contain an "id" segment in the path.
-        public IEnumerable<User> GetAllUsers()
-        {
-            return repository.GetAll();
-        }
-
         //
         // GET api/users/:id
         //
@@ -37,53 +26,12 @@ namespace PISServer.Controllers
         // correct data type (int) for the parameter.
         public User GetUser(int id)
         {
-            User item = repository.Get(id);
-            if (item == null)
+            User user = repository.Get(id);
+            if (user == null)
             {
                 throw new HttpResponseException(HttpStatusCode.NotFound);
             }
-            return item;
-        }
-
-        //
-        // POST api/users
-        //
-        public HttpResponseMessage PostUser(User item)
-        {
-            item = repository.Add(item);
-            var response = Request.CreateResponse<User>(HttpStatusCode.Created, item);
-
-            string uri = Url.Link("DefaultApi", new { id = item.Id });
-            response.Headers.Location = new Uri(uri);
-            return response;
-        }
-
-        //
-        // PUT api/users/:id
-        //
-        public void PutUser(int id, User user)
-        {
-            user.Id = id;
-            if (!repository.Update(user))
-            {
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            }
-        }
-
-        //
-        // DELETE api/users/5
-        //
-        // @param [Int] id This indicates the id of the user.
-
-        public void DeleteUser(int id)
-        {
-            User item = repository.Get(id);
-            if (item == null)
-            {
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            }
-
-            repository.Remove(id);
+            return user;
         }
     }
 }
