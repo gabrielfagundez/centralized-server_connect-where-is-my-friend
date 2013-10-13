dde429c76d3da9730cc581301dca0f283fc65bb4
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-10-13 15:52:56 -0200

    Agregar y obtener posicion agregados

--- PISServer/Controllers/GeolocationController.cs
+++ PISServer/Controllers/GeolocationController.cs
@@ -6,26 +6,69 @@ using System.Net.Http;
 using System.Web.Http;
 
 using PisDataAccess;
+using PISServer.Models.Datatypes;
 
 namespace PISServer.Controllers
 {
     public class GeolocationController : ApiController
     {
         [HttpPost]
-        public string SendLocation()
+        public LocationAddRequest SetLocation([FromBody] LocationAddRequest request)
         {
             using (var context = new DevelopmentPISEntities())
             {
-                return "Method Not Implemented";
+                if (request.Mail == null || request.Longitude == null || request.Latitude == null)
+                {
+                    throw new HttpResponseException(HttpStatusCode.NotFound);
+                }
+
+                // Find the user
+                var user = context.Users
+                            .Where(u => u.Mail == request.Mail)
+                            .FirstOrDefault();
+
+                if (user == null)
+                {
+                    throw new HttpResponseException(HttpStatusCode.NotFound);
+                }
+
+                UserPosition up = new UserPosition();
+                up.Latitude = request.Latitude;
+                up.Longitude = request.Longitude;
+
+                user.UserPosition = up;
+                context.SaveChanges();
+
+                return request;
             }
         }
 
         [HttpPost]
-        public string GetFriendLocation()
+        public Location GetFriendLocation([FromBody] UserEmailRequest request)
         {
             using (var context = new DevelopmentPISEntities())
             {
-                return "Method Not Implemented";
+                if (request.Mail == null)
+                {
+                    throw new HttpResponseException(HttpStatusCode.NotFound);
+                }
+
+                // Find the user
+                var user = context.Users
+                            .Where(u => u.Mail == request.Mail)
+                            .FirstOrDefault();
+
+                if (user == null)
+                {
+                    throw new HttpResponseException(HttpStatusCode.NotFound);
+                }
+
+                Location l = new Location();
+
+                l.Latitude = user.UserPosition.Latitude;
+                l.Longitude = user.UserPosition.Longitude;
+
+                return l;
             }
         }
     }
---
+++ PISServer/Models/Datatypes/LocationAddRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PISServer.Models.Datatypes
+{
+    public class LocationAddRequest
+    {
+        public string Mail { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
