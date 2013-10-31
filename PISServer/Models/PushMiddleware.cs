b6cfe26c1b77c8c6af11b23c3a3808c1915f9ac5
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-10-31 21:37:26 -0200

    Agregada funcion auxiliar para las push

--- PISServer/Models/PushMiddleware.cs
+++ PISServer/Models/PushMiddleware.cs
@@ -9,17 +9,31 @@ namespace PISServer.Models
 {
     public class PushMiddleware
     {
-        // Returns all the events that should be sent
-        //public string GetUnsentEvents()
-        //{
-        //}
-        //
-        //public string GetAcceptedEvents()
-        //{
-        //}
-        //
-        //public string GetRejectedEvents()
-        //{
-        //}
+        // Returns all the events that should be sent to accept/negate
+        public List<WhereSolicitationEvent> GetUnsentEvents()
+        {
+            using (var context = new DevelopmentPISEntities())
+            {
+                return context.EventSet.OfType<WhereSolicitationEvent>().Where(s => s.Sent == false).ToList();
+            }
+        }
+
+        // Return all accepted requests
+        public List<WhereAcceptationEvent> GetAcceptedEvents()
+        {
+            using (var context = new DevelopmentPISEntities())
+            {
+                return context.EventSet.OfType<WhereAcceptationEvent>().Where(s => s.Sent == false).ToList();
+            }
+        }
+
+        // Return all rejected requests
+        public List<WhereNegationEvent> GetRejectedEvents()
+        {
+            using (var context = new DevelopmentPISEntities())
+            {
+                return context.EventSet.OfType<WhereNegationEvent>().Where(s => s.Sent == false).ToList();
+            }
+        }
     }
 }
