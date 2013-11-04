124c90223ab8c93181ecbfb44124615b8e7b7678
gmordecki <gmordecki@gmail.com>
2013-11-04 14:14:52 -0200

    Creada la estrucura basica del worker role

--- PushWorkerRole/WorkerRole.cs
+++ PushWorkerRole/WorkerRole.cs
@@ -32,9 +32,10 @@ namespace PushWorkerRole
             while (true)
             {
                 PushMiddleware pm = new PushMiddleware();
-                List<WhereAcceptationEvent> wae = pm.GetAcceptedEvents();
-
-
+                pm.GetAcceptedEvents().ToList().ForEach(SendAcceptations);
+                pm.GetRejectedEvents().ToList().ForEach(SendRejections);
+                pm.GetUnsentEvents().ToList().ForEach(SendSolicitations);
+                
                 Thread.Sleep(10000);
                 //Trace.TraceInformation("Working", "Information");
             }
@@ -53,7 +54,7 @@ namespace PushWorkerRole
             return base.OnStart();
         }
 
-        private void SendNotification(Event ev)
+        private void SendAcceptations(WhereAcceptationEvent ev)
         {
             List<Session> ls = ev.User.Session.Where(s => s.Active = true)
                                 .ToList();
@@ -61,26 +62,87 @@ namespace PushWorkerRole
             {
                 if (s.Platform.Equals("android"))
                 {
-                    push.QueueNotification(new GcmNotification()    //.WithData(new Dictionary())
-                                         .ForDeviceRegistrationId(s.DeviceId)
-                                         .WithJson("{\"alert\":\"Fuiste pulliado, cabrón!\",\"badge\":7,\"sound\":\"sound.caf\"}"))
-                                          ;
+                    SendAndroid(ev, s.DeviceId, "something");
                 }
                 else if(s.Platform.Equals("wp"))
                 {
-                    push.QueueNotification(new WindowsPhoneToastNotification()
-                    .ForEndpointUri(new Uri(s.DeviceId))
-                    .ForOSVersion(WindowsPhoneDeviceOSVersion.Eight)
-                    .WithBatchingInterval(BatchingInterval.Immediate)
-                    .WithText1("You have a new solicitation!")
-                    .WithText2("Answer it!"));
+                    //No se si es ese ev.User.Name el nombre del que acepta pero esa era la idea
+                    SendWp(ev, s.DeviceId, ev.User.Name+" accepted your solicitation", "You can see where he is!", new Uri("uri"));
                 }
                 else if(s.Platform.Equals("ios"))
                 {
-                    
+                    SendIosAceptation(ev, s.DeviceId);
                 }
             }
-           
+            ev.Sent = true;
+        }
+
+        private void SendRejections(WhereNegationEvent ev)
+        {
+            List<Session> ls = ev.User.Session.Where(s => s.Active = true)
+                                .ToList();
+            foreach (Session s in ls)
+            {
+                if (s.Platform.Equals("android"))
+                {
+                    SendAndroid(ev, s.DeviceId, "something");
+                }
+                else if (s.Platform.Equals("wp"))
+                {
+                    //No se si es ese ev.User.Name el nombre del que acepta pero esa era la idea
+                    SendWp(ev, s.DeviceId, ev.User.Name + " couldn't accept your solicitation", "", new Uri("uri"));
+                }
+                else if (s.Platform.Equals("ios"))
+                {
+                    SendIosAceptation(ev, s.DeviceId);
+                }
+            }
+            ev.Sent = true;
+        }
+
+        private void SendSolicitations(WhereSolicitationEvent ev)
+        {
+            List<Session> ls = ev.User.Session.Where(s => s.Active = true)
+                                .ToList();
+            foreach (Session s in ls)
+            {
+                if (s.Platform.Equals("android"))
+                {
+                    SendAndroid(ev, s.DeviceId, "something");
+                }
+                else if (s.Platform.Equals("wp"))
+                {
+                    //No se si es ese ev.User.Name el nombre del que acepta pero esa era la idea
+                    SendWp(ev, s.DeviceId, ev.User.Name + " wants to know where you are", "Answer his solicitation!", new Uri("uri"));
+                }
+                else if (s.Platform.Equals("ios"))
+                {
+                    SendIosAceptation(ev, s.DeviceId);
+                }
+            }
+            ev.Sent = true;
+        }
+
+        private void SendAndroid(Event wae, string devId, string text)
+        {
+            push.QueueNotification(new GcmNotification()    //.WithData(new Dictionary())
+                                     .ForDeviceRegistrationId(devId)
+                                     .WithJson(text));
+        }
+
+        private void SendWp(Event wae, string devId, string text1, string text2, Uri uri)
+        {
+            push.QueueNotification(new WindowsPhoneToastNotification()
+                    .ForEndpointUri(new Uri(devId))
+                    .ForOSVersion(WindowsPhoneDeviceOSVersion.Eight)
+                    .WithBatchingInterval(BatchingInterval.Immediate)
+                    .WithText1(text1)
+                    .WithText2(text2));
+        }
+
+        private void SendIosAceptation(Event wae, string devId)
+        {
+            //not implemented yet
         }
     }
 }
